f48774cc6385125ddada245920f8c259d9248c97
Vu Van Dung <me@joulev.dev>
2022-11-13 19:20:43 +0800

    add source code link
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
       <head />
       <body>
         <RootLayoutClient>{children}</RootLayoutClient>
+        <a
+          href="https://github.com/joulev/nextjs13-router-events"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View source on GitHub
+        </a>
       </body>
     </html>
   );
