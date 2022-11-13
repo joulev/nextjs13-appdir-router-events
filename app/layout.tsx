2054117f2e0de7159eb841d7c8576aff82e3325e
Vu Van Dung <me@joulev.dev>
2022-11-13 19:24:00 +0800

    add credit for the author of the idea
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,15 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
       <head />
       <body>
         <RootLayoutClient>{children}</RootLayoutClient>
-        <a
-          href="https://github.com/joulev/nextjs13-router-events"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View source on GitHub
-        </a>
+        <div>
+          <a href="https://github.com/joulev/nextjs13-router-events">View source on GitHub</a>
+        </div>
+        <div>
+          <a href="https://github.com/vercel/next.js/discussions/41745#discussioncomment-4127651">
+            Original idea
+          </a>{" "}
+          by <a href="https://github.com/ErAz7">@ErAz7</a>
+        </div>
       </body>
     </html>
   );
