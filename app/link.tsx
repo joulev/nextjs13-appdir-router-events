b79e4fc42d3da210b41a7af10624244f89a367fd
Vu Van Dung <me@joulev.dev>
2023-04-03 11:54:58 +0800

    handle modified Link onClick events
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- app/link.tsx
+++ app/link.tsx
@@ -3,6 +3,20 @@
 import NextLink from "next/link";
 import { onStart } from "~/router-events";
 
+// https://github.com/vercel/next.js/blob/400ccf7b1c802c94127d8d8e0d5e9bdf9aab270c/packages/next/src/client/link.tsx#L169
+function isModifiedEvent(event: React.MouseEvent): boolean {
+  const eventTarget = event.currentTarget as HTMLAnchorElement | SVGAElement;
+  const target = eventTarget.getAttribute("target");
+  return (
+    (target && target !== "_self") ||
+    event.metaKey ||
+    event.ctrlKey ||
+    event.shiftKey ||
+    event.altKey || // triggers resource download
+    (event.nativeEvent && event.nativeEvent.which === 2)
+  );
+}
+
 export default function Link({ href, onClick, ...rest }: React.ComponentProps<"a">) {
   const useLink = href && href.startsWith("/");
   if (useLink)
@@ -10,8 +24,10 @@ export default function Link({ href, onClick, ...rest }: React.ComponentProps<"a
       <NextLink
         href={href}
         onClick={(event) => {
-          const { pathname, search, hash } = window.location;
-          if (href !== pathname + search + hash) onStart();
+          if (!isModifiedEvent(event)) {
+            const { pathname, search, hash } = window.location;
+            if (href !== pathname + search + hash) onStart();
+          }
           if (onClick) onClick(event);
         }}
         {...rest}
